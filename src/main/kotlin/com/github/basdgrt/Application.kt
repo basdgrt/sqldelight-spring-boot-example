8cdf337aee3e6a1002a53337577eae2e140e27e9
Bas de Groot <bas.degroot@edsn.nl>
2025-10-16 15:43:08 +0200

    Fix application class name

--- src/main/kotlin/com/github/basdgrt/Application.kt
+++ src/main/kotlin/com/github/basdgrt/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SqldelightSpringBootExampleApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<SqldelightSpringBootExampleApplication>(*args)
+	runApplication<Application>(*args)
 }
