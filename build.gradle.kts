b54d8bc241b3c04c8c8e3b9dc052d4f85f8f09a0
Bas de Groot <bas.degroot@edsn.nl>
2025-10-16 14:49:38 +0200

    Configure Postgres datasource

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+	runtimeOnly("org.postgresql:postgresql")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
